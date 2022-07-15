6843d32bf7868dff23f52cf9941f304e4dbd1393
Yuriy Kanaev <selltpc@gmail.com>
2022-07-15 09:50:03 +0700

    yaer_4,100,400

--- src/com/dmdev/task/Task4.java
+++ src/com/dmdev/task/Task4.java
@@ -2,10 +2,11 @@ package com.dmdev.task;
 
 public class Task4 {
     public static void main(String[] args) {
-        int year = 1992;
-        if (year % 4 ==0){
+        int year = 1900;
+        if (year % 400 == 0 || (year % 4 ==0 && year % 100 != 0)){
             System.out.println(year + " Високосный");
-
+        } else{
+            System.out.println(year + " Не Високосный");
         }
     }
 }
