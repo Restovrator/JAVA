670588c374075390c90ec6daa591bf470aa0ef45
Yuriy Kanaev <selltpc@gmail.com>
2022-07-15 13:48:38 +0700

    task_func_GetMax

---
+++ src/com/dmdev/task/func.java
@@ -0,0 +1,18 @@
+package com.dmdev.task;
+// вывод наибольшего числа из двух
+public class func {
+    public static void main(String[] args) {
+        int value1 = 5;
+        int value2 = 10;
+        int result = GetMax (value2, value1);
+        System.out.println(result);
+    }
+    public static int GetMax(int FirstValue, int SecondValue){
+        if (FirstValue > SecondValue){
+            return FirstValue;
+        } else {
+            return SecondValue;
+        }
+
+    }
+}
